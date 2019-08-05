b4c1e246cd3665939ce4e87b9338476c6ee377ed
Henry Pham <phamtiensy8696@gmail.com>
2019-08-05 21:47:01 +0700

    CURD Mongodb with MVC

---
+++ IoT.Web/Models/MongoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IoT.Web.Models
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _mongoDb;
+        public MongoDbContext()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _mongoDb = client.GetDatabase("IoT");
+        }
+        public IMongoCollection<Employee> Employee
+        {
+            get
+            {
+                return _mongoDb.GetCollection<Employee>("Employee");
+            }
+        }
+    }
+}
